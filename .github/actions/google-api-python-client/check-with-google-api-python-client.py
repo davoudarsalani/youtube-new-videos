fc724e76e2e53c80b24ee17cd83c081d611494db
Davoud Arsalani <d_arsalani@yahoo.com>
2021-12-30 13:31:39 +0330

    updated

--- .github/actions/google-api-python-client/check-with-google-api-python-client.py
+++ .github/actions/google-api-python-client/check-with-google-api-python-client.py
@@ -11,10 +11,6 @@ from urllib.request import urlopen
 from googleapiclient.discovery import build
 from youtube_dl import YoutubeDL
 
-script_args = argv[1:]
-message_text = ''
-urls = []
-
 
 def getopts() -> None:
     global count_last, channel_id, last_file, titles_file, api_key
@@ -42,81 +38,90 @@ def getopts() -> None:
             api_key = arg
 
 
-getopts()
+def main():
+    getopts()
 
-try:
-    youtube = build('youtube', 'v3', developerKey=api_key)
-    request = youtube.channels().list(part='statistics', id=channel_id)
-    response = request.execute()
-    count = int(response['items'][0]['statistics']['videoCount'])
+    try:
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        request = youtube.channels().list(part='statistics', id=channel_id)
+        response = request.execute()
+        count = int(response['items'][0]['statistics']['videoCount'])
 
-    ## get new count
-    ## already got in try
+        ## get new count
+        ## already got in try
 
-    ## save count as last to compare in our next read
-    with open(last_file, 'w') as opened_last_file:
-        opened_last_file.write(f'{str(count)}\n')
+        ## save count as last to compare in our next read
+        with open(last_file, 'w') as opened_last_file:
+            opened_last_file.write(f'{str(count)}\n')
 
-    ## compare
-    diff = count - count_last
-    if diff > 0:
+        ## compare
+        diff = count - count_last
+        if diff > 0:
 
-        ## download gp.py to use convert_second function (borrowed from download.py script)
-        gp_response = urlopen('https://raw.githubusercontent.com/davoudarsalani/scripts/master/gp.py')
-        with open(f'./.github/actions/google-api-python-client/gp.py', 'wb') as opened_gp:
-            for chunk in iter(partial(gp_response.read, 8192), b''):  ## 8192 is 8KB
-                opened_gp.write(chunk)
-        ## now a sloppy trick to remove everything other than convert_second function to prevent ModuleNotFoundError:
-        ## step 1: remove everythin before pattern
-        run("sed -i -n '/^def convert_second/,$p' ./.github/actions/google-api-python-client/gp.py", shell=True)
-        ## step 2: remove everything after pattern (2,$ means ignore the first occurrence)
-        ##         (had to put the commad inside {} to prevent the error: sed: -e expression #1, char 4: unknown command: `/')
-        ##         (https://stackoverflow.com/questions/6869449/skipping-the-first-n-lines-when-using-regex-with-sed)
-        run("sed -i '2,${ /^def /,$d }' ./.github/actions/google-api-python-client/gp.py", shell=True)
-        ## now we can import
-        from gp import convert_second
+            ## download gp.py to use convert_second function (borrowed from download.py script)
+            gp_response = urlopen('https://raw.githubusercontent.com/davoudarsalani/scripts/master/gp.py')
+            with open(f'./.github/actions/google-api-python-client/gp.py', 'wb') as opened_gp:
+                for chunk in iter(partial(gp_response.read, 8192), b''):  ## 8192 is 8KB
+                    opened_gp.write(chunk)
+            ## now a sloppy trick to remove everything other than convert_second function to prevent ModuleNotFoundError:
+            ## step 1: remove everythin before pattern
+            run("sed -i -n '/^def convert_second/,$p' ./.github/actions/google-api-python-client/gp.py", shell=True)
+            ## step 2: remove everything after pattern (2,$ means ignore the first occurrence)
+            ##         (had to put the commad inside {} to prevent the error: sed: -e expression #1, char 4: unknown command: `/')
+            ##         (https://stackoverflow.com/questions/6869449/skipping-the-first-n-lines-when-using-regex-with-sed)
+            run("sed -i '2,${ /^def /,$d }' ./.github/actions/google-api-python-client/gp.py", shell=True)
+            ## now we can import
+            from gp import convert_second
 
-        ## get urls
-        try:
-            request = youtube.search().list(part='snippet', order='date', channelId=channel_id)
-            response = request.execute()
+            ## get urls
+            try:
+                request = youtube.search().list(part='snippet', order='date', channelId=channel_id)
+                response = request.execute()
 
-            videos = response['items']
+                videos = response['items']
 
-            for i in range(diff):
-                video = videos[i]
-                url = video['id']['videoId']
-                url = f'https://www.youtube.com/watch?v={url}'
-                urls.append(url)
+                for i in range(diff):
+                    video = videos[i]
+                    url = video['id']['videoId']
+                    url = f'https://www.youtube.com/watch?v={url}'
+                    urls.append(url)
 
-            ## googleapiclient only gives us title and url
-            ## so for duration we have to use youtube_dl
+                ## googleapiclient only gives us title and url
+                ## so for duration we have to use youtube_dl
 
-            # tor_proxy = 'socks5://127.0.0.1:9050'
-            options = {
-                # 'proxy': tor_proxy,
-                'no_color': True,
-                'skip_download': True,
-            }
+                # tor_proxy = 'socks5://127.0.0.1:9050'
+                options = {
+                    # 'proxy': tor_proxy,
+                    'no_color': True,
+                    'skip_download': True,
+                }
 
-            ## get title and duration
-            for u in urls:
-                with YoutubeDL(options) as opened_youtubedl:
-                    # opened_youtubedl.cache.remove()
-                    video_obj = opened_youtubedl.extract_info(u, download=False)
-                    title = video_obj['title']
-                    vid_duration = video_obj['duration']  ## 41
-                    vid_duration = convert_second(vid_duration)  ## 00:00:41
+                ## get title and duration
+                for u in urls:
+                    with YoutubeDL(options) as opened_youtubedl:
+                        # opened_youtubedl.cache.remove()
+                        video_obj = opened_youtubedl.extract_info(u, download=False)
+                        title = video_obj['title']
+                        vid_duration = video_obj['duration']  ## 41
+                        vid_duration = convert_second(vid_duration)  ## 00:00:41
 
-                    message_text = f'{message_text}{title}\n{vid_duration}\n{u}\n\n'
+                        message_text = f'{message_text}{title}\n{vid_duration}\n{u}\n\n'
 
-            ## write info to titles_file
-            message_text = f'{message_text.strip()}\n'
-            with open(titles_file, 'w') as opened_titles_file:
-                opened_titles_file.write(message_text)
+                ## write info to titles_file
+                message_text = f'{message_text.strip()}\n'
+                with open(titles_file, 'w') as opened_titles_file:
+                    opened_titles_file.write(message_text)
 
-        except Exception as exc:
-            print(f'ERROR: {exc!r}')
+            except Exception as exc:
+                print(f'ERROR: {exc!r}')
 
-except Exception as exc:
-    print(f'ERROR: {exc!r}')
+    except Exception as exc:
+        print(f'ERROR: {exc!r}')
+
+
+if __name__ == '__main__':
+    script_args = argv[1:]
+    message_text = ''
+    urls = []
+
+    main()
